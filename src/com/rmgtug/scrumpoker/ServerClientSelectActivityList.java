48eea219b01b8d5e0249c6df3d0fa96106e92ebc
moehring.n@gmail.com <moehring.n@gmail.com@13b3bb21-b434-faed-a303-1158de18fb75>
2012-02-18 16:16:27 +0000

    - fixed typo

---
+++ src/com/rmgtug/scrumpoker/ServerClientSelectActivityList.java
@@ -0,0 +1,30 @@
+package com.rmgtug.scrumpoker;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class ServerClientSelectActivityList extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.server_client_select);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		Intent intent = new Intent();
+		if (position == 0) {
+			// FIXME: check if fragments available
+			intent.setClass(getApplicationContext(), AndroidScrumPokerActivity.class);
+		} else if (position == 1) {
+			intent.setClass(getApplicationContext(), ClientAndroidScumPokerActivity.class);
+		}
+		startActivity(intent);
+	}
+}
