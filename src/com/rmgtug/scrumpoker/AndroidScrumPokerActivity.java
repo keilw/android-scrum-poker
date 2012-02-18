22e974f3a6e0f875ffb101cd6287afa14470ae67
moehring.n@gmail.com <moehring.n@gmail.com@13b3bb21-b434-faed-a303-1158de18fb75>
2012-02-18 10:54:33 +0000

    Initial import.

---
+++ src/com/rmgtug/scrumpoker/AndroidScrumPokerActivity.java
@@ -0,0 +1,13 @@
+package com.rmgtug.scrumpoker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidScrumPokerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
