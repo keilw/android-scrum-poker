9f0283640adc347053dd4c841ab07c288409bbbb
mahachemer@googlemail.com <mahachemer@googlemail.com@13b3bb21-b434-faed-a303-1158de18fb75>
2012-02-19 09:26:32 +0000

    added broadcast button for server

--- src/com/rmgtug/scrumpoker/ASPListFragment.java
+++ src/com/rmgtug/scrumpoker/ASPListFragment.java
@@ -12,23 +12,25 @@ import com.rmgtug.scrumpoker.adapter.SessionListAdapter;
 public class ASPListFragment extends Fragment {
 
 	protected SessionListAdapter sla;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
-		String[] q = {"Stefan","Mario", "Norbert"};
+		String[] q = { "Stefan", "Mario", "Norbert" };
 		sla = new SessionListAdapter(getActivity().getBaseContext());
-		for(String s :q) {
+		for (String s : q) {
 			sla.addSessionItem(new SessionInfo(s, "dummyImage"));
 		}
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.session_list, container, false);
 		ListView lv = (ListView) v.findViewById(R.id.sessionlist);
 		lv.setAdapter(sla);
+
 		return v;
 	}
+
 }
--- src/com/rmgtug/scrumpoker/AndroidScrumPokerActivity.java
+++ src/com/rmgtug/scrumpoker/AndroidScrumPokerActivity.java
@@ -1,10 +1,23 @@
 package com.rmgtug.scrumpoker;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
-public class AndroidScrumPokerActivity extends Activity {
+import android.app.Activity;
+import android.content.Context;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+public class AndroidScrumPokerActivity extends Activity implements OnClickListener {
 
 	ArrayAdapter<String> aa;
 
@@ -13,5 +26,41 @@ public class AndroidScrumPokerActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.masterboard);
+		Button myButton = (Button) findViewById(R.id.btnBroadcast);
+		myButton.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Broadcast like there is no tomorrow!
+		String data = "test1343";
+		Log.d("com.rmgtug.scrumpoker", "Broadcast, my ass!");
+		DatagramSocket socket;
+		try {
+			socket = new DatagramSocket(1337);
+
+			socket.setBroadcast(true);
+			DatagramPacket packet = new DatagramPacket(data.getBytes(), data.length(), getBroadcastAddress(), 1338);
+			socket.send(packet);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	InetAddress getBroadcastAddress() throws IOException {
+		WifiManager wifi = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+		DhcpInfo dhcp = wifi.getDhcpInfo();
+		// handle null somehow
+
+		int broadcast = (dhcp.ipAddress & dhcp.netmask) | ~dhcp.netmask;
+		byte[] quads = new byte[4];
+		for (int k = 0; k < 4; k++)
+			quads[k] = (byte) ((broadcast >> k * 8) & 0xFF);
+		return InetAddress.getByAddress(quads);
 	}
 }
